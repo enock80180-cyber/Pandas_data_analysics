845b560569444572ec9262da6f7bcac2d782b7f2
enock80180-cyber <enock80180@icloud.com>
2026-08-06 08:52:17 +0200

    Create 02_loading_real_data.py

---
+++ 02_loading_real_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# 1. Real DATASET URL
+file_url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+
+# Pull the data into panda
+df = pd.read_csv(file_url)
+
+# Get the Data
+print(df.head())
